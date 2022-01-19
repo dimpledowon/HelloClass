6184d583417aff01f2f5fae1e059be63a22e52bf
dimpledowon <dowon1102@naver.com>
2022-01-19 12:46:11 +0900

    20220119
    HelloClass

--- Hello_Class/Assets/Animal.cs
+++ Hello_Class/Assets/Animal.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animal : MonoBehaviour
+public class Animal
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 동물에 대한 변수
+    public string name;
+    public string sound;
 
-    // Update is called once per frame
-    void Update()
+    //울음소리를 재생하는 메서드
+    public void playSound()
     {
-        
+        Debug.Log(name + " : " + sound);
     }
 }
